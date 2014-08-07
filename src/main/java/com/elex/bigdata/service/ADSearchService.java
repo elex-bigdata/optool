5d9da02df4d5c5bda48ba71d726c9dc06e8b9c81
griffinl <lq0987@126.com>
2014-08-07 19:05:35 +0800

    update

--- src/main/java/com/elex/bigdata/service/ADSearchService.java
+++ src/main/java/com/elex/bigdata/service/ADSearchService.java
@@ -106,7 +106,9 @@ public class ADSearchService {
         HTableInterface hTable = null;
         byte[] fm = Bytes.toBytes("basis");
         byte[] qf = Bytes.toBytes("c");
-        byte[] gidCol = Bytes.toBytes("gid");
+        byte[] aid = Bytes.toBytes("aid");
+        byte[] widthCol = Bytes.toBytes("w");
+        byte[] heightCol = Bytes.toBytes("h");
         try{
             hTable = HBaseUtil.getHTable(tableName);
             Scan scan = new Scan();
@@ -114,8 +116,8 @@ public class ADSearchService {
             scan.setStartRow(startStopRow[0]);
             scan.setStopRow(startStopRow[1]);
 //            scan.setFilter(new KeyOnlyFilter());
-            scan.addColumn(fm,qf);
-            scan.addColumn(fm,gidCol);
+            scan.addFamily(fm);
+
             scan.setCaching(1000);
             scan.setTimeRange(startTime, endTime);
 
@@ -132,8 +134,19 @@ public class ADSearchService {
                     String uid = Bytes.toString(Bytes.tail(r.getRow(), r.getRow().length - 11));
                     long time = r.getColumnLatest(fm,qf).getTimestamp();
 //                    long time = Bytes.toLong(Bytes.head(Bytes.tail(r.getRow(),r.getRow().length-3),8));
-                    String gid = Bytes.toString(r.getColumnLatest(fm,gidCol).getValue());
-                    debugLines.add(uid + " " + Constant.dfmt.format(new Date(time)) + " " + Bytes.toString(r.getColumnLatest(fm,qf).getValue()) + " " + gid);
+                    String gid = Bytes.toString(r.getColumnLatest(fm, aid).getValue());
+
+                    String detail = "{";
+                    if(r.getColumnLatest(fm, widthCol) != null){
+                        detail += "w:" + Bytes.toString(r.getColumnLatest(fm, widthCol).getValue()) + ",";
+                    }
+
+                    if(r.getColumnLatest(fm, heightCol) != null){
+                        detail += "h:" + Bytes.toString(r.getColumnLatest(fm, heightCol).getValue());
+                    }
+                    detail += "}";
+
+                    debugLines.add(uid + " " + Constant.dfmt.format(new Date(time)) + " " + Bytes.toString(r.getColumnLatest(fm,qf).getValue()) + " " + gid + ":" + detail);
                 }
 
                 count ++;
