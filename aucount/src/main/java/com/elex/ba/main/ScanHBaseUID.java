2f724baae522c68322f78153368b0dddc52bd9c8
griffinl <lq0987@126.com>
2014-08-11 13:58:32 +0800

    update

--- aucount/src/main/java/com/elex/ba/main/ScanHBaseUID.java
+++ aucount/src/main/java/com/elex/ba/main/ScanHBaseUID.java
@@ -1,17 +1,18 @@
 package com.elex.ba.main;
 
-import com.elex.ba.job.UIDCombineJob;
 import com.elex.ba.util.Utils;
+import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.ZooKeeperConnectionException;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.filter.KeyOnlyFilter;
 import org.apache.hadoop.hbase.util.Bytes;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 import java.util.concurrent.*;
 
 /**
@@ -21,38 +22,101 @@ import java.util.concurrent.*;
  */
 public class ScanHBaseUID {
 
-    public static void main(String[] args){
+    private BasicDataSource ds;
+
+    public static void main(String[] args) throws Exception{
 
         String pid = args[0];
         String startKey = args[1];
         String endKey = args[2];
 
-
-
         ExecutorService service = Executors.newFixedThreadPool(16);
-        List<Future<List<String>>> tasks = new ArrayList<Future<List<String>>>();
+        List<Future<List<Long>>> tasks = new ArrayList<Future<List<Long>>>();
 
         for(int i=0;i<16;i++){
             tasks.add(service.submit(new ScanUID("node" + i,pid,startKey,endKey)));
         }
 
-        for(Future<List<String>> uids : tasks){
+        Set<Long> allUid = new HashSet<Long>();
+
+        int all = 0;
+
+        for(Future<List<Long>> uids : tasks){
             try{
-                for(String str : uids.get()){
-                    System.out.println(str);
-                }
+                List<Long> rs = uids.get();
+                all += rs.size();
+                allUid.addAll(rs);
             }catch(Exception e){
                 e.printStackTrace();
             }
         }
 
+        ScanHBaseUID shu = new ScanHBaseUID();
+
+        Map<Long,String> result = shu.executeSqlTrue(pid,allUid);
+
+        for(String s : result.values()){
+            System.out.println(s);
+        }
+        System.out.println("All count : "  + all);
+        System.out.println("Uniq count : " + allUid.size());
         service.shutdownNow();
 
     }
 
+    public Map<Long, String> executeSqlTrue(String projectId, Set<Long> uids) throws SQLException {
+        if (uids.size() == 0) {
+            return null;
+        }
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        StringBuilder sql = new StringBuilder("select t.id, t.orig_id from `vf_");
+        sql.append(projectId);
+        sql.append("`.id_map as t where t.id in (?");
+        char comma = ',';
+        for (int i = 1; i < uids.size(); i++) {
+            sql.append(comma);
+            sql.append("?");
+        }
+        sql.append(')');
+        Map<Long, String> idmap = new HashMap<Long, String>(uids.size());
+        try {
+            conn = ds.getConnection();
+            pstmt = conn.prepareStatement(sql.toString());
+            int i = 1;
+            for(long uid : uids){
+                pstmt.setLong(i,uid);
+                i++;
+            }
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                idmap.put(rs.getLong(1), rs.getString(2));
+            }
+        } finally {
+            conn.close();
+            pstmt.close();
+            rs.close();
+        }
+
+        return idmap;
+    }
+
+    private ScanHBaseUID() {
+        ds = new BasicDataSource();
+        Collection<String> initSql = new ArrayList<String>(1);
+        initSql.add("select 1;");
+        ds.setConnectionInitSqls(initSql);
+
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUsername("xingyun");
+        ds.setPassword("xa");
+        ds.setUrl("jdbc:mysql://65.255.35.134");
+    }
+
 }
 
-class ScanUID implements Callable<List<String>>{
+class ScanUID implements Callable<List<Long>>{
 
     String node;
     String tableName;
@@ -67,7 +131,7 @@ class ScanUID implements Callable<List<String>>{
     }
 
     @Override
-    public List<String> call() throws Exception {
+    public List<Long> call() throws Exception {
         Configuration conf = HBaseConfiguration.create();
         conf.set("hbase.zookeeper.quorum", node);
         conf.set("hbase.zookeeper.property.clientPort", "3181");
@@ -81,11 +145,11 @@ class ScanUID implements Callable<List<String>>{
 
         HTable table = new HTable(conf,"deu_" + tableName);
         ResultScanner scanner = table.getScanner(scan);
-        List<String> results = new ArrayList<String>();
+        List<Long> results = new ArrayList<Long>();
         try{
             for(Result r : scanner){
                 long uid = Utils.transformerUID(Bytes.tail(r.getRow(), 5));
-                results.add(uid + "");
+                results.add(Utils.truncate(uid));
             }
         }finally {
             scanner.close();
--- aucount/src/main/java/com/elex/ba/util/Utils.java
+++ aucount/src/main/java/com/elex/ba/util/Utils.java
@@ -74,6 +74,41 @@ public class Utils {
         return result;
     }
 
+    public static byte[] toBytes(long val) {
+        byte[] b = new byte[8];
+        for (int i = 7; i > 0; i--) {
+            b[i] = (byte) val;
+            val >>>= 8;
+        }
+        b[0] = (byte) val;
+        return b;
+    }
+
+    public static long toLong(byte[] bytes) throws Exception {
+        return toLong(bytes, 0, 8);
+    }
+
+    public static long toLong(byte[] bytes, int offset, final int length) throws Exception {
+        if (length != 8 || offset + length > bytes.length) {
+            throw new Exception(
+                    "to long exception " + "offset " + offset + " length " + length + " bytes.len " + bytes.length);
+        }
+        long l = 0;
+        for (int i = offset; i < offset + length; i++) {
+            l <<= 8;
+            l ^= bytes[i] & 0xFF;
+        }
+        return l;
+    }
+
+    public static Long truncate(long hashedUID) throws Exception {
+        byte[] bytes, newBytes;
+        bytes = toBytes(hashedUID);
+        newBytes = new byte[bytes.length];
+        System.arraycopy(bytes, 4, newBytes, 4, 4);
+        return toLong(newBytes);
+    }
+
     public static String getHBaseUIDPath(String node,String pid){
         return "/user/hadoop/offline/node/" + node + "/" + pid;
     }
